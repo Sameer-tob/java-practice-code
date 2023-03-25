1c8f07eaee6de402ba239b446f521a677e67f660
Sameer <sameermonarch279@gmail.com>
2023-03-25 22:38:11 +0530

    Name Algorithms

--- kadaneAlgorithm.java
+++ kadaneAlgorithm.java
@@ -3,30 +3,45 @@ import java.util.*;
 public class kadaneAlgorithm {
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
+
+        //Size of Array
         System.out.println("Enter the Size of the Array");
         int n=scn.nextInt();
 
+        //Add element in the Array
         System.out.println("Add Element to the array");
         int arr[]=new int[n];
         for(int i=0;i<n;i++){
             arr[i]=scn.nextInt();
         }
 
+        //Display the Enetered Array
         System.out.println("Entered Array");
         for(int i=0;i<n;i++){
             System.out.print(arr[i] + " ");
         }
 
-        System.out.println("\nMax subarray sum : ");
+
+        System.out.println("\nMax subarray sum : " + maxSubarray(arr,n));
+    }
+    
+    static int maxSubarray(int arr[],int n){
+        
+        // Initialize maxSum with minimum value
         int maxSum=Integer.MIN_VALUE;
+
+        //initilize currentSum with value 0
         int currSum=0;
+        
+        //iterate over each array element to find which subarray has maximum sum
         for(int i=0;i<n;i++){
             currSum=currSum+arr[i];
             if(currSum>maxSum)
             maxSum=currSum;
+            
             if(currSum<0)
             currSum=0;
         }
-        System.out.println(maxSum);
+        return maxSum;
     }
 }
---
+++ mooreVotingAlgorithm.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class mooreVotingAlgorithm {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+
+        //Size of Array
+        System.out.println("Enter Size of Array");
+        int n=scn.nextInt();
+
+        //Add Element to the Array
+        System.out.println("Add Element to the Array");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+           arr[i]=scn.nextInt();
+        }
+
+        //Display the Entered Array
+        System.out.println("Entered Array");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+ " ");
+        }
+
+        //invoke findMajority function to compute the majority Element
+        findMajority(arr,n);
+
+        //Return the Element that Appear greater than n/2 times
+        System.out.println("\nThe majority element that appear more that >n/2 times is : " + findMajority(arr,n));
+    }
+
+    public static int findMajority(int arr[],int n){     
+
+        //Initialize initial count with value 0
+        int count=0;
+
+        //let the maximum element that appear will be 0
+        int element=0;
+        for(int i=0;i<n;i++){
+
+    /*
+         Initially the count wiil be 0, So let assume that the first Element is the Majority Element ( arr[0] is maxElement)
+         and increase the count value to 1, now i value increase(i++ ; i=2 now i for loop) and check for the condition,
+         whether that element is our assumed maxElement or not if it is then increse the count value ,if not then decrease the count value
+         keep on goint till last elent of array        
+ 
+    */
+            if(count==0){
+                element=arr[i];
+                count=1;
+            }
+            else if(arr[i]==element){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+
+        //once we get the confirmed majority element the chech for the no of times it appeard int that array
+        int maxFrequency=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==element){
+                maxFrequency++;
+            }
+        }
+        //if it appear more that n/2 time then that is the majority element
+        if(maxFrequency>n/2){
+            return element;
+        }
+        return -1;
+    }
+}
