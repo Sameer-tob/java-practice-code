e92d0d2e6b50030678f556e1739bfbc4647d94dc
Sameer <sameermonarch279@gmail.com>
2023-04-02 16:51:43 +0530

    Add Element to the Array

---
+++ twoArray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class twoArray {
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+        System.out.println("Size of Array n x m");
+        int n=scn.nextInt();
+        int m=scn.nextInt();
+
+        System.out.println("Add Element to the Array");
+        int arr[][]=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=scn.nextInt();
+            }
+        }
+
+        System.out.println("Entered Array : ");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+        }
+    }
+}
