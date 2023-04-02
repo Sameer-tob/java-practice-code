4677b3f53443770be0a5a46051e6fe6963b2bfd1
Sameer <sameermonarch279@gmail.com>
2023-04-02 17:05:31 +0530

    Add Element to the 2-D Array

--- twoArray.java
+++ twoArray.java
@@ -18,7 +18,7 @@ public class twoArray {
         System.out.println("Entered Array : ");
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                System.out.print(arr[i][j] + " ");
+                System.out.println(arr[i][j] + " ");
             }
         }
     }
