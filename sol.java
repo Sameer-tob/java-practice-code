1783a49c970953c72fa26d5d2660c6771aedd423
Sameer <sameermonarch279@gmail.com>
2023-04-08 09:17:19 +0530

    testing the git repo

--- sol.java
+++ sol.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class sol {
     public static void main(String[] args) {
-        
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Enter Size of Array");
+        int n=scn.nextInt();
+
+        System.out.println("Add Element to the Array");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+
+        System.out.println("Entered Array");
+        for(int i=0;i<n;i++){
+           System.out.print(arr[i] + " ");
+        }
     }
 }
