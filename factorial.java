fa0f652c5405ba7d78f913492808270309f25d59
Sameer <sameermonarch279@gmail.com>
2023-04-09 13:58:43 +0530

    factorial of a Number

--- factorial.java
+++ factorial.java
@@ -4,19 +4,24 @@ import java.util.Scanner;
 public class factorial {
     public static void main(String[] args) {
         Instant start=Instant.now();
+
         try (Scanner scn = new Scanner(System.in)) {
+        
             System.out.println("Enter the no ");
             int n=scn.nextInt();
 
             System.out.println(fac(n));
         }
+        
         Instant end=Instant.now();
         Duration timeElapsed = Duration.between(start, end);
         System.out.println("\nTotal time taken to execute this code : " + timeElapsed.toSeconds() + " milliseconds");
     }
+    
     static  long  fac(int n){
-       if(n==0 || n==1)
-       return 1;
+        //base cndition
+        if(n==0 || n==1)
+        return 1;
 
        return n*fac(n-1);
     }
