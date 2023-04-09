a5c5d4e250e7a6160d46f4fad106458fdca63c19
Sameer <sameermonarch279@gmail.com>
2023-04-09 13:40:47 +0530

    How to add element to an Array

--- addarray.java
+++ addarray.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class addarray {
     public static void main(String[] args) {
         try (Scanner scn = new Scanner(System.in)) {
-            System.out.println("Enter the no of element to add in array");
+            System.out.println("Enter the size of an array");
             int n=scn.nextInt();
 
+            //Add elemen to the array
             System.out.println("Add element to the Array");
             int arr[]=new int[n];
             for(int i=0;i<n;i++){
