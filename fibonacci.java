4415ed1d05585958e96338d7a66ae23d85db3bf4
Sameer <sameermonarch279@gmail.com>
2023-04-09 13:54:28 +0530

    Fibonacci series of a number

--- fibonacci.java
+++ fibonacci.java
@@ -7,31 +7,28 @@ public class fibonacci {
             int n=scn.nextInt();
 
 /*       System.out.println("Fibonacci no :  " +fib(n));        
-   }
-   static int fib(int n){
-      if(n<=1)
-      return 1;
+   
+        static int fib(int n){
+            if(n<=1)
+            return 1;
       
-      return fib(n-1)+ fib(n-2);
+           return fib(n-1)+ fib(n-2);
 */
    
-   int a=0;
-   int b=1;
-   System.out.print("1 ");
-   for(int i=1;i<n;i++){
-            if(n<1){
+            int a=0;
+            int b=1;
+            System.out.print("1 ");
+            for(int i=1;i<n;i++){
+                if(n<1){
                 
+                }            
+                else if(n>=1){
+                    int c=a+b;
+                    System.out.print(c + " ");
+                    a=b;
+                    b=c;
+                }
             }
-            
-            else if(n>=1){
-                int c=a+b;
-                System.out.print(c + " ");
-                a=b;
-                b=c;
-            }
-   }
-        }
-
-      
+        }      
     }
 }
