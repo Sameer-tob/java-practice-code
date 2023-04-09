fcc949ade24d992c009239964f965da7a134a9ec
Sameer <sameermonarch279@gmail.com>
2023-04-09 20:38:30 +0530

    HCF and LCM of two number

--- gcdlcm.java
+++ gcdlcm.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class gcdlcm {
     public static void main(String[] args) {
         try (Scanner scn = new Scanner(System.in)) {
+
             System.out.println("Enter the First no");
             long fn=scn.nextLong();
             System.out.println("Enter the Second no");
             long sn=scn.nextLong();
 
-            long ofn=fn;
-            long osn=sn;
+            long ofn=fn;  //duplicate value
+            long osn=sn;  //duplicate value
             
             while(fn>0 && sn>0){
                 if(fn>sn)
