1e27492880a011886657c8816037a275bc605294
Sameer <sameermonarch279@gmail.com>
2023-04-09 20:44:24 +0530

    Maximum subArray sum

--- kadaneAlgorithm.java
+++ kadaneAlgorithm.java
@@ -2,27 +2,26 @@ import java.util.*;
 
 public class kadaneAlgorithm {
     public static void main(String[] args) {
-        Scanner scn=new Scanner(System.in);
+        try (Scanner scn = new Scanner(System.in)) {
+            //Size of Array
+            System.out.println("Enter the Size of the Array");
+            int n=scn.nextInt();
 
-        //Size of Array
-        System.out.println("Enter the Size of the Array");
-        int n=scn.nextInt();
+            //Add element in the Array
+            System.out.println("Add Element to the array");
+            int arr[]=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=scn.nextInt();
+            }
 
-        //Add element in the Array
-        System.out.println("Add Element to the array");
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=scn.nextInt();
+            //Display the Entered Array
+            System.out.println("Entered Array");
+            for(int i=0;i<n;i++){
+                System.out.print(arr[i] + " ");
+            }
+
+            System.out.println("\nMax subarray sum : " + maxSubarray(arr,n));
         }
-
-        //Display the Enetered Array
-        System.out.println("Entered Array");
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i] + " ");
-        }
-
-
-        System.out.println("\nMax subarray sum : " + maxSubarray(arr,n));
     }
     
     static int maxSubarray(int arr[],int n){
