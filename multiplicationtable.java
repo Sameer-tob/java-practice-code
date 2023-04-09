300a198b1e28b95518ca1c67122288a7d78ea735
Sameer <sameermonarch279@gmail.com>
2023-04-09 21:12:10 +0530

    Code for table of any number

--- multiplicationtable.java
+++ multiplicationtable.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class multiplicationtable {
     public static void main(String[] args) {
         try (Scanner scn = new Scanner(System.in)) {
+            
             System.out.println("Enter the no of which you want to print table\n");
             int table=scn.nextInt();
 
