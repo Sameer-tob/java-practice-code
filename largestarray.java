b156191c96db73061213e52bbdd65af4b70c28f6
Sameer <sameermonarch279@gmail.com>
2023-04-09 20:42:20 +0530

    Largest Element in an Array

--- largestarray.java
+++ largestarray.java
@@ -17,7 +17,7 @@ public class largestarray {
                 arr[i]=scn.nextInt();
             }
 
-            //
+            //Assume largest no as arr[0]
             int largest=arr[0];
             for(int i=0;i<arr.length;i++){
                 if(arr[i]>largest){
